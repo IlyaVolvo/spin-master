2883bcd83c42b6ec941d3cab5205a3042710a39e
Ilya Volvovski <ilya@volvovski.com>
2026-05-06 21:28:31 -0500

    Modified one config default

--- server/prisma/migrations/20260506043000_system_config/migration.sql
+++ server/prisma/migrations/20260506043000_system_config/migration.sql
@@ -26,7 +26,7 @@ INSERT INTO "system_config" (
   '{"minimumPasswordLength":6,"passwordResetTokenTtlHours":1}',
   '{"defaultTournamentOffsetDays":1,"defaultTournamentTime":"18:00","registrationDeadlineOffsetMinutes":30,"cancelReasonPresets":["Tournament cancelled by organizer","Not enough registered players","Schedule conflict","Venue unavailable","Weather or emergency closure"]}',
   '{"ratingInputMin":0,"ratingInputMax":9999,"suspiciousRatingMin":800,"suspiciousRatingMax":2100}',
-  '{"roundRobin":{"minPlayers":3,"maxPlayers":32},"playoff":{"minPlayers":4,"seedDivisor":4},"swiss":{"minPlayers":6,"pairByRating":true,"maxRoundsDivisor":2},"multiRoundRobins":{"minPlayers":5,"minGroupSize":3,"minGroups":2},"preliminary":{"groupSizeMin":3,"groupSizeMax":12,"groupSizeDefault":4,"finalRoundRobinSizeDefault":6,"reservedFinalSpotsForAutoQualified":6},"matchScore":{"min":0,"max":4,"allowEqualScores":false}}',
+  '{"roundRobin":{"minPlayers":3,"maxPlayers":32},"playoff":{"minPlayers":4,"seedDivisor":4},"swiss":{"minPlayers":6,"pairByRating":true,"maxRoundsDivisor":2},"multiRoundRobins":{"minPlayers":6,"minGroupSize":3,"minGroups":2},"preliminary":{"groupSizeMin":3,"groupSizeMax":12,"groupSizeDefault":4,"finalRoundRobinSizeDefault":6,"reservedFinalSpotsForAutoQualified":6},"matchScore":{"min":0,"max":4,"allowEqualScores":false}}',
   '{"tournamentsListCacheTtlMs":30000,"socketReconnectionDelayMs":1000,"socketReconnectionAttempts":5}'
 )
 ON CONFLICT ("id") DO NOTHING;
